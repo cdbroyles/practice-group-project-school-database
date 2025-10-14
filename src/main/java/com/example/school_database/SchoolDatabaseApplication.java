6422556461cb3b5d40718fc90f5d3a0e0ce99892
cdbroyles <cdbroyles1986@gmail.com>
2025-10-14 18:25:41 -0500

    testing

---
+++ src/main/java/com/example/school_database/SchoolDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.school_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/school_database/SchoolDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
